15895bb852777a8363be8c0710e879afc1d52b4a
Khairul <95ahmadkhairul@gmail.com>
2025-08-09 22:25:50 +0700

    test connection

--- db/db.ts
+++ db/db.ts
@@ -5,8 +5,18 @@ const pool = new Pool({
   connectionString:
     process.env.APP_DATA_CONN_STRING ||
     "postgres://postgres:00000000@localhost:5432/explorer",
-  port: 5432,
   ssl: { rejectUnauthorized: false },
 });
+
+// Debug koneksi
+(async () => {
+  try {
+    const client = await pool.connect();
+    console.log("✅ Database connected successfully!");
+    client.release();
+  } catch (err: unknown) {
+    console.error("❌ Failed to connect to database:", err);
+  }
+})();
 
 export const db = drizzle(pool, { logger: true });
